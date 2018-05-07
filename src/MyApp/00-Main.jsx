0eaf8922e67d64efb27b99023f89e85b723dd254
Edxael <zerphyr.r@gmail.com>
2018-05-07 13:54:58 -0600

    code for text editor

--- src/MyApp/00-Main.jsx
+++ src/MyApp/00-Main.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 import './StyleSheets/Main.css'
 // import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../../node_modules/jquery/dist/jquery'
 import { HashRouter, Route, Link } from 'react-router-dom'
 
-import CKEditor from "react-ckeditor-component"
+// import CKEditor from "react-ckeditor-component"
 
  
 import Home from './01-Home'
 import TexEdit from './02-TextEdit'
-import Graphs from './03-Graphs'
+import Text2 from './03-Text2'
 import Hash from './04-Hashing'
 
 export default class extends React.Component{
@@ -22,7 +21,7 @@ export default class extends React.Component{
                 <div className="menu1">
                     <Link className="menuButton" to="/">Home</Link>
                     <Link className="menuButton" to="/1">TextEdit</Link>
-                    <Link className="menuButton" to="/2">Graphs</Link>
+                    <Link className="menuButton" to="/2">Text2</Link>
                     <Link className="menuButton" to="/3">Hash</Link>
                 </div>
         
@@ -30,7 +29,7 @@ export default class extends React.Component{
         
                 <Route exact path="/" component={Home}/>
                 <Route path="/1" component={TexEdit}/>
-                <Route path="/2" component={Graphs}/>
+                <Route path="/2" component={Text2}/>
                 <Route path="/3" component={Hash}/>
        
             </div>
--- src/MyApp/02-TextEdit.jsx
+++ src/MyApp/02-TextEdit.jsx
@@ -1,65 +1,59 @@
 import React from 'react'
-import CKEditor from "react-ckeditor-component"  // https://www.npmjs.com/package/react-ckeditor-component
+import CKEditor from "react-ckeditor-component"     // https://www.npmjs.com/package/react-ckeditor-component
+var ReactDOMServer = require('react-dom/server');   // https://www.npmjs.com/package/html-to-react
+var HtmlToReactParser = require('html-to-react').Parser;
 
 
  
 export default class extends React.Component {
-    constructor(props) {
-        super(props);
-        this.updateContent = this.updateContent.bind(this);
-        this.state = {
-            content: '',
-        }
-    }
+    state = { content: 'Type Here', show: false }
  
     updateContent(newContent) {
-        this.setState({
-            content: newContent
-        })
+        this.setState({ content: newContent })
     }
     
     onChange(evt){
-      console.log("onChange fired with event info: ", evt);
-      var newContent = evt.editor.getData();
-      this.setState({
-        content: newContent
-      })
+        var newContent = evt.editor.getData();
+        console.log("Lo escrito: ", newContent)
+        this.setState({ content: newContent })
     }
-    
-    onBlur(evt){
-      console.log("onBlur event called with event info: ", evt);
-    }
-    
-    afterPaste(evt){
-      console.log("afterPaste event called with event info: ", evt);
-    }
-
-
 
     render() {
 
-        const printToConsole = () => {
-            console.log(this.state.content)
+        const newHTML = () => {
+            const textOut = { textAlign: "left", backgroundColor: "rgb(230, 249, 252)", padding: "15px", border: "1px solid black" }
+            var htmlToReactParser = new HtmlToReactParser();
+            var MyReactElement = htmlToReactParser.parse(this.state.content);
+
+            return  <div style={ textOut } >{ MyReactElement }</div>
         }
+
 
         return(
             <div>
                 <h1>Text Editor</h1>
                 
                 <CKEditor 
-                activeClass="p10" 
-                content={this.state.content} 
-                events={{
-                    "blur": this.onBlur,
-                    "afterPaste": this.afterPaste,
-                    "change": this.onChange
-                }}
+                    activeClass="p10" 
+                    content={this.state.content} 
+                    events={{
+                        "change": this.onChange.bind(this)
+                    }}
                 />
 
                 <br/>
-
-                <button className="gralButton" >Print Text</button>
+                <hr/>
+                <br/>
+  
+                    { React.createElement(newHTML) }
+ 
+                <div></div>
+                
             </div>
         )
     }
 }
+
+
+
+// button className="gralButton" onClick={ printToConsole } >Print Text</button>
